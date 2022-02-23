582e6d00ea931b1d4649cc4c85a64f04f06312f3
DDev <79892178+DivyaDev1992@users.noreply.github.com>
2022-02-23 14:44:51 +0530

    Routing

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'home',
+  template: `
+    <div>
+      <h3>Child Page</h3>
+    </div>
+  `,
+})
+export class ChildComponent implements OnInit {
+    operation: string | undefined;
+    constructor(private route: ActivatedRoute) {}
+    ngOnInit() {
+      this.route.params.subscribe((params) => this.operation = params['operation']);
+      alert(this.operation)
+    }
+  }
