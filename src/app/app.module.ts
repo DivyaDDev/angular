bfaa1f9176da8abfbaddcb2d392b5b55cfcecf2e
DDev <79892178+DivyaDev1992@users.noreply.github.com>
2022-02-25 09:40:01 +0530

    Routing in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ParentHttpService } from './parent/parent.service';
-import { ResponseInterceptor } from './utility/response.interceptor';
-import { HeaderInterceptor } from './utility/header.interceptor';
-import { ErrorInterceptor } from './utility/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -15,14 +10,9 @@ import { ErrorInterceptor } from './utility/error.interceptor';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [ 
-    { provide: HTTP_INTERCEPTORS, useClass: ResponseInterceptor, multi: true }, 
-    { provide: HTTP_INTERCEPTORS, useClass: HeaderInterceptor, multi: true }, 
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }, 
-    ParentHttpService],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
